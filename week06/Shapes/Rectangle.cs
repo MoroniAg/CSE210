b8471dadf14ebd60246b51b8f747e2c38b26b345
MoroniAg <moroniaguilera1993@gmail.com>
2026-06-09 14:20:10 +0000

    Add Circle, Rectangle, Square classes and update Program to display shape details

---
+++ week06/Shapes/Rectangle.cs
@@ -0,0 +1,17 @@
+class Rectangle: Shape
+{
+    private double _width;
+    private double _length;
+
+    public Rectangle(string color, double width, double length) : base(color)
+    {
+        _width = width;
+        _length = length;
+    }
+
+    public override double getArea()
+    {
+        return _width * _length;
+    }
+    
+}
